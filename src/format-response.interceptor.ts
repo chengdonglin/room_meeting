62a75f59859ebfb862d46bd3b2cb2db44f8355d1
linchengdong <linchengdong@dm-ai.cn>
2023-09-06 11:14:43 +0800

    feat: 响应内容的拦截器

---
+++ src/format-response.interceptor.ts
@@ -0,0 +1,28 @@
+/*
+ * @Description:
+ * @Date: 2023-09-06 10:43:43
+ */
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+import { Response } from 'express';
+
+@Injectable()
+export class FormatResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse<Response>();
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          code: response.status,
+          message: 'success',
+          data,
+        };
+      }),
+    );
+  }
+}
