688a76ead4e6c18a2721fd29d7762b36eb517d76
linchengdong <linchengdong@dm-ai.cn>
2023-09-06 11:20:04 +0800

    feat: 请求记录日志拦截器

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FormatResponseInterceptor } from './format-response.interceptor';
+import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalInterceptors(new InvokeRecordInterceptor());
+  // LoginGuard 里从 jwt 取出来放到 request.user 的，而 Guard 在 interceptor 之前调用：
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));
 }
